79ed918420da54cbeac1f4a050111b803dade638
Yuliya-14 <ulialosakova509@gmail.com>
2022-09-29 21:17:26 +0300

    init

---
+++ HW1.py
@@ -0,0 +1,10 @@
+user_name = input('Введите Ваше имя: ')
+age = int(input('Введите Ваш год рождения: '))
+current_age = 2022 - age
+message = 'Привет,'
+print(f'{message} {user_name}. Сейчас Вам лет: {current_age}')
+
+user_name1 = input('Your name:')
+print(f'{message} {user_name1}!')
+
+print('*********\n*       *\n*       *\n*********')
