bfb62d6ef70e687253c60d7ce8b80362d91c0306
Yuliya-14 <ulialosakova509@gmail.com>
2022-10-12 00:05:48 +0300

    Мои методы

--- my_methods.py
+++ my_methods.py
@@ -1,3 +1,10 @@
 line = "\033[92m--*-----*-----*-----*-----*--\033[0m"
 
 end = '\033[93m\033[1mВсе задания выполнены\033[0m'
+
+def div(x,y):
+    try:
+        return x / y
+    except ZeroDivisionError:
+        return 'Can`t divide by zero'
+
