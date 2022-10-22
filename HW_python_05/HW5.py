2c2238334efcbab4f43c0a1d569fbcf9d4f8442c
Yuliya-14 <ulialosakova509@gmail.com>
2022-10-22 18:52:27 +0300

    HW5

---
+++ HW_python_05/HW5.py
@@ -0,0 +1,17 @@
+import my_methods
+
+class Dogs:
+    def __init__(self, nickname, age):
+        self.nickname = nickname
+        self.age = age
+
+    def
+
+class Breed(Dogs):
+
+dog = Dogs('Piff', 3)
+print(dog.nickname)
+print(dog.age)
+
+
+print(my_methods.line)
