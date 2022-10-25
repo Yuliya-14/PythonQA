dcf30a41aa24c52bf003d5a30cc59b493cd49ceb
Yuliya-14 <ulialosakova509@gmail.com>
2022-10-25 21:56:39 +0300

    HW5

--- HW_python_05/HW5.py
+++ HW_python_05/HW5.py
@@ -11,6 +11,15 @@ class Dogs:
     def vaccine(self):
         return "Собака идет на привику :("
 
+    def animal_dogs(self):
+        return "Animals is beautiful"
+
+    def get_age(self):
+        return f"My dog is {self._age}"
+
+    def set_age(self, newage):
+        self._age = newage
+
 class Breed(Dogs):
     def __init__(self, nickname, age, breed):
         super().__init__(nickname, age)
@@ -18,6 +27,10 @@ class Breed(Dogs):
 
     def sleeps(self):
         return "Собака любит спать!!"
+
+    def animal_dogs(self):
+        return "sleeps"
+
 
 class Color(Dogs):
     def __init__(self, nickname, age, breed, color):
@@ -28,12 +41,16 @@ class Color(Dogs):
     def hairstyle(self):
         return "Собака идет на стрижку :)"
 
+    def animal_dogs(self):
+        return "Style"
+
 
 dog1 = Dogs('Piff', 3)
 print(dog1.nickname)
 print(dog1.age)
 print(dog1.plays())
 print(dog1.vaccine())
+print(dog1.animal_dogs())
 
 print(my_methods.line)
 
@@ -44,6 +61,7 @@ print(dog2.breed)
 print(dog2.sleeps())
 print(dog2.plays())
 print(dog2.vaccine())
+print(dog2.animal_dogs())
 
 print(my_methods.line)
 
@@ -55,6 +73,9 @@ print(dog3.color)
 print(dog3.plays())
 print(dog3.hairstyle())
 print(dog3.vaccine())
+print(dog3.animal_dogs())
+
+print(my_methods.line)
 
 
 
